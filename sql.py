93395b39c3f95a0a9f03d1b5b324aa367bfc66c3
Horvath Mate <ifjmate@gmail.com>
2018-02-21 22:03:24 +0100

    Create sql query that gets the password corresponding to the give username

--- sql.py
+++ sql.py
@@ -7,3 +7,12 @@ def register_user(cursor, username, password_hash):
     INSERT INTO users
     VALUES (DEFAULT, %(username)s, %(password_hash)s)
     """, {"username": username, "password_hash": password_hash})
+
+
+@sql_connection.connection_handler
+def login_user(cursor, username):
+    cursor.execute("""
+    SELECT password_hash FROM users
+    WHERE username = %(username)s
+    """, {"username": username})
+    return cursor.fetchone()
