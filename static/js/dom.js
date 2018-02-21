babf026bd3529a3ea49dea0753be35469caa6098
Horvath Mate <ifjmate@gmail.com>
2018-02-21 15:40:58 +0100

    Made modal size bigger and made modal more responsive

--- static/js/dom.js
+++ static/js/dom.js
@@ -34,7 +34,7 @@ let dom = {
             if (val["residents"].length > 0) {
                 let modal = `
             <div class="modal" id="${val["name"]}Modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog" role="document">
+              <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                   <div class="modal-header">
                     <h5 class="modal-title" id="${val["name"]}ModalLabel">Resident of ${val["name"]}</h5>
