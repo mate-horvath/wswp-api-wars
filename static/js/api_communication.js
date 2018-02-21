6abd2aeee77c81f3d1da4c0094ae44c65e8b107f
Horvath Mate <ifjmate@gmail.com>
2018-02-21 12:50:09 +0100

    Add _data porperty to apiHandler

--- static/js/api_communication.js
+++ static/js/api_communication.js
@@ -1,9 +1,11 @@
 let apiHandler = {
+    _data: {},
     getAllData: function (callback, url) {
         $.ajax({
             dataType: "json",
             url: url,
             success: function(response) {
+                this._data = response;
                 callback(response);
         }
 });
