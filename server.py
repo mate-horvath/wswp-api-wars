5dc519e0ce0560bd39bbbb7a671c1d4e5ffe784b
Horvath Mate <ifjmate@gmail.com>
2018-02-21 20:32:04 +0100

    Create route for login, logout, registration

--- server.py
+++ server.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+import sql
+import data_manager
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template("index.html")
 
 
+@app.route('/registration', methods=["GET", "POST"])
+def registration():
+    if request.method == "POST":
+        username = request.form['username']
+        password = data_manager.hash_password(request.form['password'][0])
+        sql.register_user(username, password)
+        return redirect(url_for("login"))
+    return render_template("registration.html")
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and 'username' not in session:
+        username = request.form['username']
+        password = request.form['password']
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(
+        host='0.0.0.0',
+        port=5000,
+        debug=True)
