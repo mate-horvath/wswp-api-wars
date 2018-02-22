c337e040debf28c30dfeaa4a6adb70ae17596da9
Horvath Mate <ifjmate@gmail.com>
2018-02-22 18:03:43 +0100

    Create sql querys to add vote data and get vote info

--- sql.py
+++ sql.py
@@ -12,7 +12,23 @@ def register_user(cursor, username, password_hash):
 @sql_connection.connection_handler
 def login_user(cursor, username):
     cursor.execute("""
-    SELECT password_hash FROM users
+    SELECT password_hash, id FROM users
     WHERE username = %(username)s
     """, {"username": username})
-    return cursor.fetchone()
+    return cursor.fetchall()
+
+
+@sql_connection.connection_handler
+def voting(cursor, vote_data):
+    cursor.execute("""
+    INSERT INTO planet_votes
+    VALUES (DEFAULT, %(submission_time)s, %(planet_name)s, %(user_id)s, %(planet_id)s)""",
+                   {"submission_time": vote_data["submission_time"], "planet_name": vote_data["planet_name"],
+                    "user_id": vote_data["user_id"], "planet_id": vote_data["planet_id"]})
+
+
+@sql_connection.connection_handler
+def count_votes(cursor):
+    cursor.execute("""SELECT planet_name, count(planet_name) AS vote_count FROM planet_votes
+                      GROUP BY planet_name;""")
+    return cursor.fetchall()
